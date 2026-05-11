40086f0878cd32ae64045bbc56dbb1064ca628e0
Deploy <deploy@render.com>
2026-05-11 07:29:12 -0500

    Fix: remove get_pool import from db/__init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,4 +1,4 @@
 # db/__init__.py
-from .conexion import Database, get_connection, get_pool, fetch_one, fetch_all, execute, db_query, db_transaction
+from .conexion import Database, get_connection, fetch_one, fetch_all, execute, db_query, db_transaction
 from .cache import Cache, pdf_cache, preview_cache, cached, invalidate
 from .validators import Validator, ValidationError, sanitize_string, sanitize_int, sanitize_float
